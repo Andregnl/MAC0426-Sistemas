338f6bbd630045cd164e1078469ccbbed67b0bca
Gabriel Alves <alves.gabriel@novaenergia.com.br>
2024-05-10 09:56:07 -0300

    initial configs

---
+++ database.js
@@ -0,0 +1,30 @@
+//postgre connection pool
+
+// db:
+//     image: postgres
+//     restart: always
+//     environment:
+//       POSTGRES_DB: mac0426db
+//       POSTGRES_USER: mac0426user
+//       POSTGRES_PASSWORD: mac0426password
+//     ports:
+//       - "5432:5432"
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mac0426db',
+    password: 'mac0426password',
+    port: 5432,
+});
+
+export async function createConnectionPool() {
+    try {
+        await pool.connect();
+        console.log("Pool de conexões criado com sucesso");
+    } catch (error) {
+        console.error("Erro ao criar o pool de conexões:", error);
+    }
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { createConnectionPool } from './database.js'
+
+const app = express()
+const port = 3000
+
+
+async function startApplication() {
+    try {
+        // Abre o pool de conexões
+        // createConnectionPool();
+        // Inicia a aplicação Express
+        app.listen(port, () => {
+            console.log("Servidor rodando na porta "+ port);
+        });
+    } catch (error) {
+        console.error("Erro ao iniciar a aplicação:", error);
+    }
+}
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  startApplication()
